38cf87f4d6e4c8ef61bee44ed3f407ea65d2b045
Jacob Ruzi <JacobRuzi@Jacobs-MacBook-Pro.local>
2019-01-27 23:36:28 -0500

    more logging

--- banderplug.py
+++ banderplug.py
@@ -1,4 +1,5 @@
 import configparser
+import logging
 import re
 
 class BanderGame:
@@ -6,7 +7,7 @@ class BanderGame:
     def __init__(self,gameFile,saveFile=None):
         self.saveFile = None
         self.stage = '0'
-        self.stages = self.__loadStages(gameFile)
+        self.__loadStages(gameFile)
         if saveFile:
             self.saveFile = saveFile
 
@@ -17,19 +18,37 @@ class BanderGame:
         gameStages = {}
 
         for stanza in stanzas:
+
+            # Check stanza has required settings
+            if 'id' not in stage.keys() and stanza!='default':
+                logging.error('The stage - %s - is incorrectly configured: missing id' % stanza)
+                exit(1)
+            elif 'message' not in stage.keys() and stanza!='default':
+                logging.error('The stage - %s - is incorrectly configured: missing message' % stanza)
+                exit(1)
+            elif ('gameEnding' not in stage.keys() and 'gameWinning' not in stage.keys()) \
+                    or ('gameEnding' not in stage.keys() and openGameFile[stanza]['gameWinning']!='True') \
+                    or ('gameWinning' not in stage.keys() and penGameFile[stanza]['gameEnding']!='True'):
+                numChoices = 0
+                choicePattern = re.compile(r"choice\.(\d+)\s+=")
+                for key in stage.keys():
+                    matching = choicePattern.match(key)
+                    if matching:
+                        numChoices += 1
+                        if 'response.%s' % matching.group(1) not in stage.keys():
+                            logging.error('The stage - %s - is missing a response for choice %s' \
+                                          % (stanza,matching.group(1)))
+                if numChoices == 0:
+                    logging.error('The stage - %s - does not have any choices' % stanza)
+
             gameStages[openGameFile[stanza]['id']] = {}
             stage = gameStages[openGameFile[stanza]['id']]
+
             for key in openGameFile[stanza]:
                 if key != 'id':
                     stage[key] = openGameFile[stanza][key]
 
-            # Check stanza has required settings
-            acceptableStage = True
-
-            if 'message' not in stage.keys():
-                acceptableStage = False
-
-        return gameStages
+        self.stages = gameStages
 
     def __presentStage(self, gameOver=False):
         stageSettings = self.stages[self.stage]
@@ -41,9 +60,15 @@ class BanderGame:
                 print('%s: %s' % (settingName[1], stageSettings[key]))
 
         if not gameOver:
-            userChoice = input('Enter the number of your selection: ')
-
-            self.stage = stageSettings['response.%s' % userChoice]
+            waitingForGoodInput = True
+            while waitingForGoodInput:
+                userChoice = input('Enter the number of your selection: ')
+                if 'response.%s' % userChoice in stageSettings:
+                    self.stage = stageSettings['response.%s' % userChoice]
+                    waitingForGoodInput = False
+                else:
+                    print('That is not a valid option.')
+                    logging.warning('The user did not select a valid choice: %s' % userChoice)
 
     def playGame(self):
         stageSettings = self.stages[self.stage]
@@ -60,6 +85,14 @@ class BanderGame:
         return str(self.stages)
 
 def main():
+    #Temporary logging settings (eventually will move this to a logging.conf file)
+    logger = logging.getLogger('banderplug')
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('BanderPlug.log')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
     myGameFile = 'testGame.conf'
     myGame = BanderGame(myGameFile)
     myGame.playGame()
